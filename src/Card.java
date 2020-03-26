a18d467df23cfceaf45b7c17bcdd77fcaf27ebbb
BranceA <brancealexander@gmail.com>
2020-03-26 12:31:26 -0500

    added shuffle method

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,35 @@
 public class Card {
     private int pointValue;
     private boolean isAce;
+    private String whatCard;
+
+    public Card(int pointValue, boolean isAce, String whatCard) {
+        this.pointValue = pointValue;
+        this.isAce = isAce;
+        this.whatCard = whatCard;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    public boolean isAce() {
+        return isAce;
+    }
+
+    public void setAce(boolean ace) {
+        isAce = ace;
+    }
+
+    public String getWhatCard() {
+        return whatCard;
+    }
+
+    public void setWhatCard(String whatCard) {
+        this.whatCard = whatCard;
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -3,7 +3,80 @@ import java.util.ArrayList;
 public class Deck {
     private ArrayList<Card> cards;
 
-    public void shuffle(){};
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void shuffle(){
+        String[] suits = {"S", "C", "D", "H"};
+        ArrayList<Card> deck = new ArrayList<>();
+
+        for(String suit : suits) {
+            for (int i = 1; i <= 13; i++) {
+                String suitAndCard = " ";
+                suitAndCard += suit;
+                switch (i) {
+                    case 1:
+                        suitAndCard += "A";
+                        deck.add(new Card(11, true, suitAndCard));
+                        break;
+                    case 2:
+                        suitAndCard += "2";
+                        deck.add(new Card(2, false, suitAndCard));
+                        break;
+                    case 3:
+                        suitAndCard += "3";
+                        deck.add(new Card(3, false, suitAndCard));
+                        break;
+                    case 4:
+                        suitAndCard += "4";
+                        deck.add(new Card(4, false, suitAndCard));
+                        break;
+                    case 5:
+                        suitAndCard += "5";
+                        deck.add(new Card(5, false, suitAndCard));
+                        break;
+                    case 6:
+                        suitAndCard += "6";
+                        deck.add(new Card(6, false, suitAndCard));
+                        break;
+                    case 7:
+                        suitAndCard += "7";
+                        deck.add(new Card(7, false, suitAndCard));
+                        break;
+                    case 8:
+                        suitAndCard += "8";
+                        deck.add(new Card(8, false, suitAndCard));
+                        break;
+                    case 9:
+                        suitAndCard += "9";
+                        deck.add(new Card(9, false, suitAndCard));
+                        break;
+                    case 10:
+                        suitAndCard += "10";
+                        deck.add(new Card(10, false, suitAndCard));
+                        break;
+                    case 11:
+                        suitAndCard += "J";
+                        deck.add(new Card(10, false, suitAndCard));
+                        break;
+                    case 12:
+                        suitAndCard += "Q";
+                        deck.add(new Card(10, false, suitAndCard));
+                        break;
+                    case 13:
+                        suitAndCard += "K";
+                        deck.add(new Card(10, false, suitAndCard));
+                        break;
+                }
+            }
+        }
+        this.cards = deck;
+    };
 
     public void dealCard(){};
 }
